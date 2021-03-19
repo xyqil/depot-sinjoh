c88eadd76615e6fd3c3bb20e21c0bc8c7bd98a22
6100m <77129415+6100m@users.noreply.github.com>
2021-03-19 15:05:26 -0600

    Update multiplexmii.js

--- site/mutliplexers/000/js/multiplexmii.js
+++ site/mutliplexers/000/js/multiplexmii.js
@@ -4,6 +4,7 @@ function initalizemii(){
   s.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"; 
   s.onload = function(e){initalizeotherscripts()};  
   document.head.appendChild(s);
+  return Math.random().toString(36).substring(2);
 };
 function initalizeotherscripts(){
   $.getScript('apis/nullapi/000/nullapi.js', function() {
@@ -18,4 +19,10 @@ function initalizeotherscripts(){
   $.getScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js', function() {
     console.log("https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js was loaded")
   });
+  initalizethecss();
+  return Math.random().toString(36).substring(2);
 };
+function initalizethecss(){
+  $('body').append('<link rel="stylesheet" href="multiplexers/000/css/multiplexmii.css" type="text/css">');
+  return Math.random().toString(36).substring(2);
+}
