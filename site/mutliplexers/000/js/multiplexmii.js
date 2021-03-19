f7ab170b12df41b2d58d9cef758e30709d7dee6b
6100m <77129415+6100m@users.noreply.github.com>
2021-03-19 14:57:49 -0600

    Update multiplexmii.js

--- site/mutliplexers/000/js/multiplexmii.js
+++ site/mutliplexers/000/js/multiplexmii.js
@@ -1,1 +1,21 @@
-window.onload = autoboot(nothing);
+window.onload = autoboot();initalizemii();
+function initalizemii(){
+  var s = document.createElement("script"); 
+  s.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"; 
+  s.onload = function(e){initalizeotherscripts()};  
+  document.head.appendChild(s);
+};
+function initalizeotherscripts(){
+  $.getScript('apis/nullapi/000/nullapi.js', function() {
+    console.log("apis/nullapi/000/nullapi.js was loaded")
+  });
+  $.getScript('apis/vidapi/000/vidapi.js', function() {
+    console.log("apis/vidapi/000/vidapi.js was loaded")
+  });
+  $.getScript('apis/bootupapi/000/bootupapi.js', function() {
+    console.log("apis/bootupapi/000/bootupapi.js was loaded")
+  });
+  $.getScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js', function() {
+    console.log("https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js was loaded")
+  });
+};
