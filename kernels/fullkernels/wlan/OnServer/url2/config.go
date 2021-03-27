40aea2d18c5a0c570f837e1670293d134be9cc65
6100m <77129415+6100m@users.noreply.github.com>
2021-03-27 14:54:17 -0600

    Update config.go

--- kernels/fullkernels/wlan/OnServer/url2/config.go
+++ kernels/fullkernels/wlan/OnServer/url2/config.go
@@ -1,6 +1,13 @@
 package main
 const (
-    id1 = "pleasechangemethankyouverymuch"
-    id2 = "pleasechangemethankyouverymuch"
-    id3 = "pleasechangemethankyouverymuch"
+    data0 = "pleasechangemethankyouverymuch"
+    data1 = "pleasechangemethankyouverymuch"
+    data2 = "pleasechangemethankyouverymuch"
+    data3 = "pleasechangemethankyouverymuch
+    data4 = "pleasechangemethankyouverymuch"
+    data5 = "pleasechangemethankyouverymuch"
+    data6 = "pleasechangemethankyouverymuch"
+    data7 = "pleasechangemethankyouverymuch"
+    data8 = "pleasechangemethankyouverymuch"
+    data9 = "pleasechangemethankyouverymuch"
 )
