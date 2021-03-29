8937a193e3c200f84b60306af5efb3f5fe6f62d6
6100m <77129415+6100m@users.noreply.github.com>
2021-03-28 22:08:15 -0600

    Update runner.go

--- kernels/fullkernels/wlan/OnServer/csd/runner.go
+++ kernels/fullkernels/wlan/OnServer/csd/runner.go
@@ -3,6 +3,7 @@ package main
 import (
 "net/http"
 )
-
+func runsite(){
 http.HandleFunc(combine(address5, key), handler)
-    http.ListenAndServe(address5, mode)
+http.ListenAndServe(address5, mode)
+}
