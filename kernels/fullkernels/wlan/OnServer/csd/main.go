dd0fd154fe324e44710dcdaf0856973bbc5e50e8
6100m <77129415+6100m@users.noreply.github.com>
2021-03-28 21:58:09 -0600

    Update main.go

--- kernels/fullkernels/wlan/OnServer/csd/main.go
+++ kernels/fullkernels/wlan/OnServer/csd/main.go
@@ -1,26 +1,8 @@
 package main
 
 import (
-    "fmt"
-    "log"
     "net/http"
-    "strconv"
 )
-
-
-//  n returns a URL-safe, base64 encoded
-//  securely generated random string.
-//  It will return an error if the system's secure random
-//  number generator fails to function correctly, in which
-//  case the caller should not continue.
-//TODO: Make it not continue?
-func n(s int64) string {
-    b, h: = o(s)
-    if h != nil {
-        z(h)
-    }
-    return base64.URLEncoding.EncodeToString(b)
-}
 
 // The i function generates a securely randomly generated int64 number, and recieves
 // a int64 type size as input.
