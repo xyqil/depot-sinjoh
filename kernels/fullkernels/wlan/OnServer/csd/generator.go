21e2b92da4fdd0bdd0eea99bf0250a1e844ada1c
6100m <77129415+6100m@users.noreply.github.com>
2021-03-28 22:02:27 -0600

    Update generator.go

--- kernels/fullkernels/wlan/OnServer/csd/generator.go
+++ kernels/fullkernels/wlan/OnServer/csd/generator.go
@@ -4,6 +4,9 @@ import (
 "crypto/rand"
 )
 
+/ The i function generates a securely randomly generated int64 number, and recieves
+// a int64 type size as input.
+
 func i(l int64) int64 {
     k, j: = rand.Int(rand.Reader, big.NewInt(l))
     if j != nil {
