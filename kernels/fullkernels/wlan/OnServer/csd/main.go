da38d17a0f3ba709e6c5faa2998e14da4985d95e
6100m <77129415+6100m@users.noreply.github.com>
2021-03-28 22:01:16 -0600

    Update main.go

--- kernels/fullkernels/wlan/OnServer/csd/main.go
+++ kernels/fullkernels/wlan/OnServer/csd/main.go
@@ -4,17 +4,6 @@ import (
     "net/http"
 )
 
-// The i function generates a securely randomly generated int64 number, and recieves
-// a int64 type size as input.
-
-func i(l int64) int64 {
-    k, j: = rand.Int(rand.Reader, big.NewInt(l))
-    if j != nil {
-        z(j)
-    }
-    return k.Int64()
-}
-
 func main() {
     http.HandleFunc(combine(address5, key), handler)
     http.ListenAndServe(address5, nil)
