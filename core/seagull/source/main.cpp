022f0344236997a6df6e710510aaee781a7f58c0
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-04-01 19:58:16 -0400

    unbreakith

--- core/seagull/source/main.cpp
+++ core/seagull/source/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int c(){for(int a=1;a<100;a++){
+int main(){for(int a=1;a<100;a++){
 if(a%15==0){cout<<"fizzbuzz"<<endl; 
 }else if(a%5==0){cout<<"fizz"<<endl;
 }else if(a%3==0){cout<<"buzz"<<endl;
