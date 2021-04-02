a4b61312649dc93226fb1ba2e033e06bdbd4370f
6100m <77129415+6100m@users.noreply.github.com>
2021-04-01 19:05:33 -0600

    Update and rename Makefile to build.php

---
+++ core/tools/binarycopierandsendertool/build.php
@@ -0,0 +1,17 @@
+<?php
+$data1 = shell_exec('export data=source/main/');
+$data2 = shell_exec('cd $$data');
+$data3 = shell_exec('rm -rf main2.go');
+$data4 = shell_exec('rm -rf main');
+$data5 = shell_exec('rm -rf ./main');
+$data6 = "pre"
+$data7 = "<"
+$data8 = ">"
+$data9 = "/pre"
+header("Content-Type:text/html"
+echo $data7+$data6+$data8+$data1+$data7+$data9+$data8
+echo $data7+$data6+$data8+$data2+$data7+$data9+$data8
+echo $data7+$data6+$data8+$data3+$data7+$data9+$data8
+echo $data7+$data6+$data8+$data4+$data7+$data9+$data8
+echo $data7+$data6+$data8+$data5+$data7+$data9+$data8
+?>
