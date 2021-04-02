eff13c70e989859665bbf8495db2afbd51addca5
6100m <77129415+6100m@users.noreply.github.com>
2021-04-02 13:10:26 -0600

    Uh, that was uh, from the article I was using. Please don't think I am drunk lol

--- core/wlan/OnServer/cryptorangeserver/index.php
+++ core/wlan/OnServer/cryptorangeserver/index.php
@@ -9,8 +9,7 @@ if ($integerdata == 1) {
 } elseif ($integerdata == 3) {
   $xml = new xml('<?xml version="1.0" encoding="ISO-8859-1"?>
 <primarynode>1</primarynode>
-<secondarynode>16384</secondarynode>
-</breakfast_menu>');
+<secondarynode>16384</secondarynode>');
   echo $xml->data
 } else {
   die();
