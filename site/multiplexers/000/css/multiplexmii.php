83e34bb7a3544ced1a48d34c18be0215af5bca54
6100m <77129415+6100m@users.noreply.github.com>
2021-04-02 16:57:14 -0600

    Update multiplexmii.php

--- site/multiplexers/000/css/multiplexmii.php
+++ site/multiplexers/000/css/multiplexmii.php
@@ -2,8 +2,8 @@
 header("Content-type: text/css");
 echo "@import url(\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css\")\n";
 echo "@import url(\"https://vjs.zencdn.net/7.10.2/video-js.min.css\")\n";
-echo "@import \"../../../apis/larsapi/000/larsapi.css\")\n";
-echo "@import \"../../../styles/000/styler.css\")\n";
-echo "@import \"../../../apis/fontapi/000/fontapi.css\")\n";
-echo "@import \"../../../apis/vidapi/001/vidapi.css\")\n";
+echo "@import \"../../../apis/larsapi/000/larsapi.php\")\n";
+echo "@import \"../../../styles/000/styler.php\")\n";
+echo "@import \"../../../apis/fontapi/000/fontapi.php\")\n";
+echo "@import \"../../../apis/vidapi/001/vidapi.php\")\n";
 ?>
