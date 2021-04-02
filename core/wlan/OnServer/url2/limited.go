851dce38f74f3cc26961c6588e36fd6a525c706d
6100m <77129415+6100m@users.noreply.github.com>
2021-04-02 13:35:23 -0600

    Create limited.go

---
+++ core/wlan/OnServer/url2/limited.go
@@ -0,0 +1,25 @@
+package main;
+
+import (
+  "database/sql"
+  "strconv"
+
+func limited() {
+  sqlStatement := `SELECT integerdata, FROM limited WHERE integerdata=$1;`
+  var integerdata int
+  var id3 int
+  var t[1] string
+  z, y := strconv.Itoa(integerdata)
+  x, u := strconv.Itoa(id3)
+  v(z)
+  v(x)
+  row := db.QueryRow(sqlStatement, u)
+  switch s := row.Scan(&integerdata); s {
+  case sql.ErrNoRows:
+    v(s)
+  case nil:
+    t[0] = integerdata
+    return t
+  default:
+    v(s)
+  }
