8ac11ed834a4c65ab644f3f34a7b7b6a50c8a835
NanhuGITHUB <77976308+PiForUAll@users.noreply.github.com>
2021-04-14 10:51:10 -0400

    don't forget to fix this code gloom

--- src/newroom/beacon.go
+++ src/newroom/beacon.go
@@ -1,27 +1,29 @@
-
-package main;
+package main
 
 import (
-  "database/sql"
-  "strconv"
+	"database/sql"
+	"strconv"
 )
 
-func limited() string{
-  var integerdata int
-  var id4 int
-  var t[1] string
-  z, y := strconv.Itoa(integerdata)
-  x, u := strconv.Itoa(id4)
-  v(z)
-  v(x)
-  n := db.QueryRow("SELECT code, msg FROM beacon WHERE integerdata=$1;", u)
-  switch s := n.Scan(&code, &msg); s {
-  case sql.ErrNoRows:
-    v(s)
-  case nil:
-    t[0] = code
-    t[1] = msg
-    return t
-  default:
-    v(s)
-  }
+/* fix this too */
+
+func limited() string {
+	var integerdata int
+	var id4 int
+	var t [1]string
+	z, y := strconv.Itoa(integerdata)
+	x, u := strconv.Itoa(id4)
+	v(z)
+	v(x)
+	n := db.QueryRow("SELECT code, msg FROM beacon WHERE integerdata=$1;", u)
+	switch s := n.Scan(&code, &msg); s {
+	case sql.ErrNoRows:
+		v(s)
+	case nil:
+		t[0] = code
+		t[1] = msg
+		return t
+	default:
+		v(s)
+	}
+}
