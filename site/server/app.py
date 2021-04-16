310c261968db0029e754ed4828ccf30f9f92f3d4
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-04-15 22:41:13 -0400

    Update app.py

--- site/server/app.py
+++ site/server/app.py
@@ -1,8 +1,21 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+  # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  # @                CAUTION:                    @ 
+  # @  This page is meant for if the server is   @
+  # @  well, unable to serve things, it's very   @
+  # @  important that no other files are         @
+  # @  included here and that there aren't       @
+  # @  any things that may prohibit the page     @
+  # @  from loading.                             @
+  # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  # *phew* I finally got that ASCII warning sign right
+  
   return '''
   <!DOCTYPE html>
   <html>
