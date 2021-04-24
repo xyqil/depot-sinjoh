ea9c5f4f7edb61796293812f57c97ce32fb1ce5a
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-04-24 14:59:46 -0400

    Update sinjohshell.sh

--- sinjohshell.sh
+++ sinjohshell.sh
@@ -9,4 +9,5 @@ then
   ./sinjohshell/Scripts/deactivate
 else
   echo "Run ./sinjohshell.sh create to create venv or run ./sinjohshell/Scripts/activate to launch enviroment. Launching venv is basic stuff :P"
+  fi
   
