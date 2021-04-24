8be8de9c7edff23a572d5bf704682e84b85aaaff
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-04-24 14:53:21 -0400

    Update and rename seagull.a.cpp to seagull.cpp

--- src/seagull.cpp
+++ src/seagull.cpp
@@ -5,3 +5,9 @@ if(a%15==0){cout<<"fizzbuzz";
 }else if(a%5==0){cout<<"fizz\n";
 }else if(a%3==0){cout<<"buzz\n";
 }else{cout<<a<<endl;}}}
+
+int main(){
+  // This exists because we have no other functions, to name main sorry!
+  // DO NOT MINIFY OR CHANGE THE NAME OF MAIN, IT WILL BREAK BUILDS
+  return 0;
+}
