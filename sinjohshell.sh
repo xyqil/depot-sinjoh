ce5be06373c91d0a118c048bcc5db03a1c7f2c4a
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-04-25 08:45:38 -0400

    Update sinjohshell.sh

--- sinjohshell.sh
+++ sinjohshell.sh
@@ -3,12 +3,10 @@ if [ $1 = create ]
 then
   echo Creating Enviroment...
   python3 -m venv sinjohshell
-  ls sinjohshell
-  ./sinjohshell/bin/activate
+  source ./sinjohshell/bin/activate
   pip install -r requirements.txt
-  chmod +x post_env_create.sh
-  ./post_env_create.sh
-  ./sinjohshell/bin/deactivate
+  source ./post_env_create.sh
+  deactivate
 else
   echo "Run ./sinjohshell.sh create to create venv or run ./sinjohshell/Scripts/activate to launch enviroment. Launching venv is basic stuff :P"
   fi
