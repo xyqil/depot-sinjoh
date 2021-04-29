0ee332f54d4410095121f0d2b56620417ae26313
zurgeg <zurgeg@gmail.com>
2021-04-29 19:43:34 -0400

    cool kidz flask moment

--- site/app.py
+++ site/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route('/oh_dear_what_a_blunder_ive_made')
-def home():
+def uhohspeghettios():
   # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   # @                CAUTION:                    @ 
   # @  This page is meant for if the server is   @
@@ -27,6 +27,25 @@ def home():
   </body>
   </html>
   '''
-
+debug = True
+if debug:
+  @app.route('/thirdparty/spectrum/vars/dist/<css>')
+  def spectrum_vars(css):
+    return send_from_directory('thirdparty/spectrum/vars/dist', css)
+  @app.route('/thirdparty/spectrum/page/dist/<css>')
+  def spectrum_page(css):
+    return send_from_directory('thirdparty/spectrum/page/dist', css)
+  @app.route('/thirdparty/spectrum/typography/dist/<css>')
+  def spectrum_typography(css):
+    return send_from_directory('thirdparty/spectrum/typography/dist', css)
+  @app.route('/thirdparty/spectrum/icon/dist/<css>')
+  def spectrum_icon(css):
+    return send_from_directory('thirdparty/spectrum/icon/dist', css)
+  @app.route('/thirdparty/spectrum/button/dist/<css>')
+  def spectrum_button(css):
+    return send_from_directory('thirdparty/spectrum/button/dist', css)
+@app.route('/')
+def home():
+  return render_template("index.html")
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=debug)
