95f044da1142a91b07f3774026f34fbcb6d169ba
6100m <77129415+6100m@users.noreply.github.com>
2021-05-04 03:22:38 +1000

    Update DOWNLOAD.sh

--- src/forecast/assembler/DOWNLOAD.sh
+++ src/forecast/assembler/DOWNLOAD.sh
@@ -1,2 +1,2 @@
-wget https://cdn.discordapp.com/attachments/783461298531860495/811022895643754496/nortfite_battlepass.zip -O $PWD/sqldata.zip
-unzip $PWD/sqldata.zip
+make downloadit --file=RUNNER.makefile
+make runit --file=RUNNER.makefile
