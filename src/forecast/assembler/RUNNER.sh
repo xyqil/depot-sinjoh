47adead27873a282018f99cf537269388d87913a
6100m <77129415+6100m@users.noreply.github.com>
2021-05-04 03:43:10 +1000

    Update RUNNER.sh

--- src/forecast/assembler/RUNNER.sh
+++ src/forecast/assembler/RUNNER.sh
@@ -1,6 +1,6 @@
-make executesetupscript --file=$PWD/EXECUTOR.makefile
-make executegrabberscript --file=$PWD/EXECUTOR.makefile
-make chmodit --file=$PWD/EXECUTOR.makefile
-make runit --file=$PWD/EXECUTOR.makefile
-make announceit --file=$PWD/EXECUTOR.makefile
-make logit --file=$PWD/EXECUTOR.makefile
+make executesetupscript
+make executegrabberscript
+make chmodit
+make runit
+make announceit
+make logit
