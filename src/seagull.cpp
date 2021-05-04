8d5ae7a35ccf13d3ce2d09779ce766ea72a87fd6
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-05-04 16:58:22 -0400

    We need this or builds will break

--- src/seagull.cpp
+++ src/seagull.cpp
@@ -6,3 +6,8 @@ if(a%15==0){cout<<"fizzbuzz";
 }else if(a%3==0){cout<<"buzz\n";
 }else if(a%100=0){return a}
 }else{cout<<a<<endl;}}}
+int main(){
+  // This exists because we have no other functions, to name main sorry!
+  // DO NOT MINIFY OR CHANGE THE NAME OF MAIN, IT WILL BREAK BUILDS
+  return 0;
+}
