9d125c8ce2cf9fd12d4e2596dc693bfc46c4db74
6100m <77129415+6100m@users.noreply.github.com>
2021-05-04 13:28:55 -0600

    Lol JG no, this is just a library for a easter egg within a homebrew app I am making xP It's not meant to be a CLI tool.

--- src/seagull.cpp
+++ src/seagull.cpp
@@ -4,10 +4,5 @@ int c(){for(int a=1;a<100;a++){
 if(a%15==0){cout<<"fizzbuzz";
 }else if(a%5==0){cout<<"fizz\n";
 }else if(a%3==0){cout<<"buzz\n";
+}else if(a%100=0){return a}
 }else{cout<<a<<endl;}}}
-
-int main(){
-  // This exists because we have no other functions, to name main sorry!
-  // DO NOT MINIFY OR CHANGE THE NAME OF MAIN, IT WILL BREAK BUILDS
-  return 0;
-}
