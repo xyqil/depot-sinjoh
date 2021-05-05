0245d71e9542535733c299768303738fccf40acd
zurgeg <zurgeg@gmail.com>
2021-05-05 18:45:01 -0400

    Fix hCaptcha defaults

--- site/config-example.py
+++ site/config-example.py
@@ -1,6 +1,6 @@
 from app import app
 app.secret_key = 'it\'s a secret to everyone'
 app.config['HCAPTCHA_ENABLED'] = not app.debug
-app.config['HCAPTCHA_PUBLIC_KEY'] = 'yadda yadda'
-app.config['HCAPTCHA_PRIVATE_KEY'] = 'yadda yadda'
+app.config['HCAPTCHA_SITE_KEY'] = 'yadda yadda'
+app.config['HCAPTCHA_SECRET_KEY'] = 'yadda yadda'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{join(getcwd(), "app.db")}'
