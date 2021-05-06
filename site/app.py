07beafa87b49dfc8698f65a0852b0ba8dca8fe66
zurgeg <zurgeg@gmail.com>
2021-05-05 20:28:21 -0400

    PLEASE WORK

--- site/app.py
+++ site/app.py
@@ -233,7 +233,7 @@ def signup():
     db.session.add(u)
     db.session.commit()
     return redirect('/home')
-  return render_template("signup.html",form=form, theme=theme, sitekey=app.config['HCAPTCHA_SITE_KEY']
+  return render_template("signup.html",form=form, theme=theme, sitekey=app.config['HCAPTCHA_SITE_KEY'])
 @app.route('/logout')
 def logoutuser():
   logout_user()
