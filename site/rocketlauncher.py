a8dc0cdd05b4f8ba09b4e0ec230f3db2bd54d12a
zurgeg <zurgeg@gmail.com>
2021-05-05 22:33:39 -0400

    More RocketLauncher Updates

--- site/rocketlauncher.py
+++ site/rocketlauncher.py
@@ -8,11 +8,11 @@ def send_message(message):
         data['content'] = message
         data['username'] = 'RocketLauncher'
         data['avatar_url'] = 'https://static.thenounproject.com/png/254007-200.png'
-        requests.post(DISCORD_WEBHOOK_URL, body=data)
+        post(DISCORD_WEBHOOK_URL, body=data)
 
 psaq = Popen(['sudo','docker','ps','-aq'], stdout = PIPE)
 psaq, _ = psaq.communicate()
-psaq = str(psaq).split(' ')
+psaq = str(psaq)[2:-1].split('\n')
 command = ['sudo', 'docker', 'stop']
 for ps in psaq:
     command.append(ps)
