cc047f00b7eaa4b16af7fbebd9fe612ff5372dea
zurgeg <zurgeg@gmail.com>
2021-05-06 09:22:38 -0400

    Fix RocketLauncher

--- site/rocketlauncher.py
+++ site/rocketlauncher.py
@@ -1,4 +1,4 @@
-from subprocess import Popen, PIPE
+from subprocess import run, PIPE
 from config import DISCORD_WEBHOOK_URL, ROCKETLAUNCHER_USE_DISCORD
 from requests import post
 
@@ -10,18 +10,18 @@ def send_message(message):
         data['avatar_url'] = 'https://static.thenounproject.com/png/254007-200.png'
         post(DISCORD_WEBHOOK_URL, json=data)
 
-psaq = Popen(['sudo','docker','ps','-aq'], stdout = PIPE).wait()
-psaq, _ = psaq.communicate()
+psaq = run(['sudo','docker','ps','-aq'], capture_output=True)
+psaq = psaq.stdout
 psaq = str(psaq)[2:-1].split('\\n')
 command = ['sudo', 'docker', 'stop']
 for ps in psaq:
     command.append(ps)
-Popen(command)
+run(command)
 send_message(':arrow_down: Pulling repository...')
-Popen(['git', 'pull']).wait()
+run(['git', 'pull'])
 send_message(':white_check_mark: Pulled repository!')
 send_message(':hammer: Building site...')
-Popen(['sudo','docker','build','.','-t','latest']).wait()
+run(['sudo','docker','build','.','-t','latest'])
 send_message(':white_check_mark: Built site!')
 send_message(':sunglasses: The site is live!')
-out = Popen(['sudo','docker','run','-dp','443:5000', '-v', 'bruh-moment:/etc/db', 'latest:latest'], stdout=PIPE).wait()
+run(['sudo','docker','run','-dp','443:5000', '-v', 'bruh-moment:/etc/db', 'latest:latest'])
