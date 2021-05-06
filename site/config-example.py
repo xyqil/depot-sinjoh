7f3266778d5ba5e74e8020526d9dfbb2f271e16d
zurgeg <zurgeg@gmail.com>
2021-05-05 22:15:26 -0400

    Create a build script

--- site/config-example.py
+++ site/config-example.py
@@ -4,3 +4,5 @@ app.config['HCAPTCHA_ENABLED'] = not app.debug
 app.config['HCAPTCHA_SITE_KEY'] = 'yadda yadda'
 app.config['HCAPTCHA_SECRET_KEY'] = 'yadda yadda'
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{join(getcwd(), "app.db")}'
+DISCORD_WEBHOOK_URL = '' # Optional: For building with rocketlauncher.py
+ROCKETLAUNCHER_USE_DISCORD = True
