4bfc08a16a0cbc458eb9b593b0f961709d553db1
zurgeg <zurgeg@gmail.com>
2021-05-24 12:43:39 -0400

    forgot to commit

---
+++ src/daemons/cachedaemon/cachedaemon/download_webasm.sh
@@ -0,0 +1,3 @@
+rm -rf /var/xc24/webasm/000000/bin/webdata/
+git clone https://github.com/nemasu/asmttpd /var/xc24/webasm/000000/bin/
+cp -r /var/xc24/webddata/000000/ cp -r /var/xc24/webasm/000000/bin/webdata
