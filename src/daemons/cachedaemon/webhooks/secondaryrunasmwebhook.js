a2c1fc0ce4e36decb7b85668db1777050555a475
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:23:47 -0600

    Create secondaryrunasmwebhook.js

---
+++ src/daemons/cachedaemon/webhooks/secondaryrunasmwebhook.js
@@ -0,0 +1,5 @@
+const config = require('config');
+const WebHookURLData = config.get('PrimaryWebhook.WebhookURL');
+const webhook = require("webhook-discord")
+const Hook = new webhook.Webhook(WebHookURLData)
+Hook.success("6100m\'s asmttpd runner","Ran!")
