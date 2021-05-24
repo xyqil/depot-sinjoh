0e137b70cd04fcc671b692441dbc8796ce5bd077
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:25:12 -0600

    Update secondarybuildasmwebhook.js

--- src/daemons/cachedaemon/webhooks/secondarybuildasmwebhook.js
+++ src/daemons/cachedaemon/webhooks/secondarybuildasmwebhook.js
@@ -1,5 +1,5 @@
 const config = require('config');
-const WebHookURLData = config.get('PrimaryWebhook.WebhookURL');
+const WebHookURLData = config.get('SecondaryBuildASMWebhook.WebhookURL');
 const webhook = require("webhook-discord")
 const Hook = new webhook.Webhook(WebHookURLData)
 Hook.success("6100m\'s asmttpd builder","Built!")
