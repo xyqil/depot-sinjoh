d7d7702117afd3b7fd3767cc162f7add2989f522
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 10:10:22 -0600

    Ok so using this Node.JS module I can dynamically pull the source code every 30 minutes. Uncertain though if that's a good amount, but I have no clue of the time it takes to compile. We will see.

---
+++ site/daemons/cachedaemon/pull.js
@@ -0,0 +1,15 @@
+var gitPullCron = require('git-pull-cron');
+ 
+/*
+- Clone given repo into /dev/my-repo, replacing what's already there
+- Schedule cron to run every weekday (Mon-Fri) at 11:30am
+- When cron task runs, a `git pull origin master` will be performed
+- Once cron task has run the callback will get invoked with latest commit info
+ */
+gitPullCron.init('git://github.com/jwilder/dockerize.git', '/var/asmweb/000000/', '00 30 11 * * 1-5', function(err, commit) {
+  if (err) {
+    return console.error(err.stack);
+  }
+ 
+  console.log('Updated to commit: ' + commit.id);
+});
