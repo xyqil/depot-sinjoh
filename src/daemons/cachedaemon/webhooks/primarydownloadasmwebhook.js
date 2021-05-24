f661b13874b0bd409e8f8b89915e77f3c8de93be
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:24:25 -0600

    Update primarydownloadasmwebhook.js

--- src/daemons/cachedaemon/webhooks/primarydownloadasmwebhook.js
+++ src/daemons/cachedaemon/webhooks/primarydownloadasmwebhook.js
@@ -1,5 +1,5 @@
 const config = require('config');
-const WebHookURLData = config.get('PrimaryWebhook.WebhookURL');
+const WebHookURLData = config.get('PrimaryDownloadASMWebhook.WebhookURL');
 const webhook = require("webhook-discord")
 const Hook = new webhook.Webhook(WebHookURLData)
 Hook.info("6100m\'s asmttpd downloader","Downloading......")
