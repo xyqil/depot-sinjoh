0125a338a947e0b23c6d19ee4c051f143862efd1
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 10:24:20 -0600

    Fix typo

--- site/daemons/cachedaemon/run.sh
+++ site/daemons/cachedaemon/run.sh
@@ -1,13 +1,13 @@
 node $PWD/pull.js
 node $PWD/primarywebhook.js
-rm -rf /var/webasm/000000/compile_dockerfile.sh
+rm -rf /var/webasm/000000/compile_dockerize.sh
 rm -rf /var/webasm/000000/secondarywebhook.js
 rm -rf /var/webasm/000000/secondarywebhookconfig.js
-cp -r $PWD/compile_dockerfile.sh /var/webasm/000000/compile_dockerfile.sh
+cp -r $PWD/compile_dockerfile.sh /var/webasm/000000/compile_dockerize.sh
 cp -r $PWD/secondarywebhook.js /var/webasm/000000/secondarywebhook.js
 cp -r $PWD/secondarywebhookconfig.js /var/webasm/000000/secondarywebhookconfig.js
 STR=/var/webasm/000000/
 cd $STR
-chmod 777 $PWD/compile_dockerfile.sh
-./compile_dockerfile.sh
+chmod 777 $PWD/compile_dockerize.sh
+./compile_dockerize.sh
 node $PWD/secondarywebhook.js
