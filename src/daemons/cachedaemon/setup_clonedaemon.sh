9b5c7155859346e2b16c2822d8d77e801b06fc6c
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:16:35 -0600

    Update setup_clonedaemon.sh

--- src/daemons/cachedaemon/setup_clonedaemon.sh
+++ src/daemons/cachedaemon/setup_clonedaemon.sh
@@ -4,7 +4,9 @@ curl -fsSL https://get.docker.com -o $PWD/installdata/get-docker.sh
 sh $PWD/installdata/get-docker.sh
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install nodejs
+node $PWD/webhooks/primaryclonedaemonsetupwebhook.js
 chmod 777 build_webasm.sh
 ./build_webasm.sh
 chmod 777 download_webasm.sh
 ./download_webasm.sh
+node $PWD/webhooks/secondaryclonedaemonsetupwebhook.js
