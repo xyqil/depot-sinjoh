5bda19d8f0b108d043b22fd190773f6f41f5a0e4
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:36:42 -0600

    Update run_webasm.sh

--- src/daemons/cachedaemon/run_webasm.sh
+++ src/daemons/cachedaemon/run_webasm.sh
@@ -1,3 +1,3 @@
-node primaryasmrunwebhook.js
+node /var/webasm/000000/bin/webhooks/primaryasmrunwebhook.js
 ./asmttpd $PWD/webdata/000000/ 60000
-node secondaryasmrunwebhook.js
+node /var/webasm/000000/bin/webhooks/secondaryasmrunwebhook.js
