22f24d676a3b96782802e3cd7d56b858c5ad913c
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:37:33 -0600

    Update build_webasm.sh

--- src/daemons/cachedaemon/build_webasm.sh
+++ src/daemons/cachedaemon/build_webasm.sh
@@ -1,3 +1,3 @@
-node /var/xc24/webasm/000000/webhooks/pprimarybuildasmwebhook.js
+node /var/xc24/webasm/000000/webhooks/primarybuildasmwebhook.js
 make release
-node /var/xc24/webasm/000000/webhooks/psecondarybuildasmwebhook.js
+node /var/xc24/webasm/000000/webhooks/secondarybuildasmwebhook.js
