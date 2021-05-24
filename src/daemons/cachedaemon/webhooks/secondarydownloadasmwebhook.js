5865e4a0acd446cb5dfc0cef376108b1213a93ff
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:25:44 -0600

    Update secondarydownloadasmwebhook.js

--- src/daemons/cachedaemon/webhooks/secondarydownloadasmwebhook.js
+++ src/daemons/cachedaemon/webhooks/secondarydownloadasmwebhook.js
@@ -1,5 +1,5 @@
 const config = require('config');
-const WebHookURLData = config.get('PrimaryWebhook.WebhookURL');
+const WebHookURLData = config.get('SecondaryDownloadASMWebhook.WebhookURL');
 const webhook = require("webhook-discord")
 const Hook = new webhook.Webhook(WebHookURLData)
-Hook.info("6100m\'s asmttpd runner","Ran!")
+Hook.success("6100m\'s asmttpd runner","Ran!")
