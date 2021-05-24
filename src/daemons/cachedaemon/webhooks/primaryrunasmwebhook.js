222125d09c2222b518d6dc987b92f1a8534e8c3f
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:24:38 -0600

    Update primaryrunasmwebhook.js

--- src/daemons/cachedaemon/webhooks/primaryrunasmwebhook.js
+++ src/daemons/cachedaemon/webhooks/primaryrunasmwebhook.js
@@ -1,5 +1,5 @@
 const config = require('config');
-const WebHookURLData = config.get('PrimaryWebhook.WebhookURL');
+const WebHookURLData = config.get('PrimaryRunASMWebhook.WebhookURL');
 const webhook = require("webhook-discord")
 const Hook = new webhook.Webhook(WebHookURLData)
 Hook.info("6100m\'s asmttpd runner","Running......")
