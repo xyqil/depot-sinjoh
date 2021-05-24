a0ef9bce0a765597e87a2553249e045cfd6d6440
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 10:36:42 -0600

    Update and rename setup.sh to setup_clonedaemon.sh

--- site/daemons/cachedaemon/setup_clonedaemon.sh
+++ site/daemons/cachedaemon/setup_clonedaemon.sh
@@ -4,4 +4,3 @@ curl -fsSL https://get.docker.com -o $PWD/installdata/get-docker.sh
 sh $PWD/installdata/get-docker.sh
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install nodejs
-git clone https://github.com/nemasu/asmttpd /var/xc24/webasm/000000/bin/webasm/
