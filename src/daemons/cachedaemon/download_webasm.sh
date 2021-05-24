5a77e0d3245301fb55f39fd4f4856e0bfcac55c7
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:42:10 -0600

    Update download_webasm.sh

--- src/daemons/cachedaemon/download_webasm.sh
+++ src/daemons/cachedaemon/download_webasm.sh
@@ -1,5 +1,7 @@
 node /var/xc24/webasm/000000/webhooks/primarydownloadasmwebhook.js
 rm -rf /var/xc24/webasm/000000/bin/webdata/
 git clone https://github.com/nemasu/asmttpd /var/xc24/webasm/000000/bin/
+cp -r $PWD/build_webasm.sh /var/xc24/webasm/000000/bin/build_webasm.sh
+cp -r $PWD/download_webasm.sh /var/xc24/webasm/000000/bin/download_webasm.sh
 cp -r /var/xc24/webddata/000000/ /var/xc24/webasm/000000/bin/webdata
 node /var/xc24/webasm/000000/webhooks/secondarydownloadasmwebhook.js
