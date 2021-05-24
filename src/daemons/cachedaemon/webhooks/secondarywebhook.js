45684f67c5c9e55f8dca3000f8dfa312658482a7
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:23:07 -0600

    Create secondarywebhook.js

---
+++ src/daemons/cachedaemon/webhooks/secondarywebhook.js
@@ -0,0 +1,5 @@
+const config = require('config');
+const WebHookURLData = config.get('PrimaryWebhook.WebhookURL');
+const webhook = require("webhook-discord")
+const Hook = new webhook.Webhook(WebHookURLData)
+Hook.info("6100m\'s dockerize grabber","Grabbed!")
