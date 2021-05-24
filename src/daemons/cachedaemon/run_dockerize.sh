f551423c1279022e3c415c67f7bbd6953c8ab4a1
6100m <77129415+6100m@users.noreply.github.com>
2021-05-24 11:39:39 -0600

    Update run_dockerize.sh

--- src/daemons/cachedaemon/run_dockerize.sh
+++ src/daemons/cachedaemon/run_dockerize.sh
@@ -3,12 +3,16 @@ node /var/xc24/webasm/000000/webhooks/pull.js
 node /var/xc24/webasm/000000/webhooks/primarywebhook.js
 rm -rf /var/webasm/000000/bin/config/default.json
 rm -rf /var/webasm/000000/bin/compile_dockerize.sh
-rm -rf /var/xc24/webasm/000000/webhooks/pprimarywebhook.js
-rm -rf /var/xc24/webasm/000000/webhooks/psecondarywebhook.js
-rm -rf /var/xc24/webasm/000000/webhooks/pprimaryrunasmwebhook.js
-rm -rf /var/xc24/webasm/000000/webhooks/psecondaryrunasmwebhook.js
-rm -rf /var/xc24/webasm/000000/webhooks/pprimarybuildasmwebhook.js
-rm -rf /var/xc24/webasm/000000/webhooks/psecondarybuildasmwebhook.js
+rm -rf /var/xc24/webasm/000000/webhooks/primarywebhook.js
+rm -rf /var/xc24/webasm/000000/webhooks/secondarywebhook.js
+rm -rf /var/xc24/webasm/000000/webhooks/primaryrunasmwebhook.js
+rm -rf /var/xc24/webasm/000000/webhooks/secondaryrunasmwebhook.js
+rm -rf /var/xc24/webasm/000000/webhooks/primarybuildasmwebhook.js
+rm -rf /var/xc24/webasm/000000/webhooks/secondarybuildasmwebhook.js
+rm -rf /var/xc24/webasm/000000/webhooks/primarydownloadasmwebhook.js
+rm -rf /var/xc24/webasm/000000/webhooks/secondarydownloadasmwebhook.js
+rm -rf /var/xc24/webasm/000000/webhooks/primarysetupasmwebhook.js
+rm -rf /var/xc24/webasm/000000/webhooks/secondarysetupasmwebhook.js
 mkdir /var/webasm/000000/bin/config/
 cp -r $PWD/webhooks/config/default.json /var/xc24/webasm/000000/webhooks/config/default.json
 cp -r $PWD/webhooks/compile_dockerfile.sh /var/xc24/webasm/000000/webhooks/compile_dockerize.sh
@@ -18,6 +22,10 @@ cp -r $PWD/webhooks/primaryrunasmwebhook.js /var/xc24/webasm/000000/webhooks/pri
 cp -r $PWD/webhooks/secondaryrunasmwebhook.js /var/xc24/webasm/000000/webhooks/secondaryrunasmwebhook.js
 cp -r $PWD/webhooks/primarybuildasmwebhook.js /var/xc24/webasm/000000/webhooks/primarybuildasmwebhook.js
 cp -r $PWD/webhooks/secondarybuildasmwebhook.js /var/xc24/webasm/000000/webhooks/secondarybuildasmwebhook.js
+cp -r $PWD/webhooks/primarydownloadasmwebhook.js /var/xc24/webasm/000000/webhooks/primarydownloadasmwebhook.js
+cp -r $PWD/webhooks/secondarydownloadasmwebhook.js /var/xc24/webasm/000000/webhooks/secondarydownloadasmwebhook.js
+cp -r $PWD/webhooks/primarysetupasmwebhook.js /var/xc24/webasm/000000/webhooks/primarysetupasmwebhook.js
+cp -r $PWD/webhooks/secondarysetupasmwebhook.js /var/xc24/webasm/000000/webhooks/secondarysetupasmwebhook.js
 mainpathdata=/var/webasm/000000/
 cd $mainpathdata
 chmod 777 $PWD/compile_dockerize.sh
